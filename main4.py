010d8860c1b3f97cf71fe65b762ab2e40f1a3793
Luisfernandogomes <lf5568487@gmail.com>
2025-03-28 14:07:41 -0300

    a

--- main4.py
+++ main4.py
@@ -18,17 +18,29 @@ def main(page: ft.Page):
     # maior de idade
 
     def maiorDeIdade(e):
-        idade_recebida = input_data_nas.value
-        data_recebida = parser.parse(idade_recebida)
-        tempo_atual = datetime.date.today()
-        ano_subtraido = tempo_atual.year -data_recebida.year
-        meses_subtraido = tempo_atual.month -data_recebida.month
-        dias_subtraido = tempo_atual.day -data_recebida.day
-        if ano_subtraido >= 18 and meses_subtraido >= 0 and dias_subtraido >= 0:
-            maior_de_idade.value = 'voce é maior de idade'
-            page.update()
-        else:
-            maior_de_idade.value = 'voce é menor de idade'
+
+        try:
+            idade_recebida = input_data_nas.value
+            data_recebida = parser.parse(idade_recebida)
+            tempo_atual = datetime.date.today()
+            idade = tempo_atual - data_recebida
+            ano_subtraido = tempo_atual.year -data_recebida.year
+            meses_subtraido = tempo_atual.month -data_recebida.month
+            dias_subtraido = tempo_atual.day -data_recebida.day
+
+            if ano_subtraido > 112:
+                maior_de_idade.value = 'data de nascimento invalido, não tem como ter mais de 120 anos'
+                page.update()
+
+            elif ano_subtraido >= 18 and meses_subtraido >= 0 and dias_subtraido >= 0:
+                maior_de_idade.value = 'voce é maior de idade'
+                page.update()
+
+            else:
+                maior_de_idade.value = 'voce é menor de idade'
+                page.update()
+        except ValueError:
+            maior_de_idade.value = "data de nascimento invalido"
             page.update()
 
 
@@ -50,6 +62,8 @@ def main(page: ft.Page):
     input_name = ft.TextField(label="digite seu nome", hint_text="digite seu nome; Exemplo: Luis Fernando")
     btn_enviar = ft.FilledButton(text="Enviar", width=page.window.width, on_click=mostrar_nome )
     txt_resultado = ft.Text(value="")
+
+
 
     input_data_nas = ft.TextField(label="insira sua data de nascimento", hint_text="digite seu ano de nascimento")
     btn_calcular = ft.FilledButton(text="calcular idade", width=page.window.width, on_click=maiorDeIdade)
---
+++ navegacao/main.py
@@ -0,0 +1,46 @@
+import flet as ft
+from flet.core.app_bar import AppBar
+from flet import AppBar, ElevatedButton, Page, Text, View
+from flet.core.colors import Colors
+from flet.core.textfield import TextField
+
+
+def main(page: Page):
+    page.title = "exemplo de rotas"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.window_width = 375
+    page.window_height = 667
+
+    def voltar(e):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+
+    def gerenciar_rotas(e):
+        page.views.clear()
+        page.views.append(
+            View(
+                "/",
+                [
+                    AppBar(title=Text("home"), bgcolor=Colors.PRIMARY_CONTAINER),
+                    ElevatedButton(text="Navegar", on_click=lambda _: page.go("/segunda"))
+                ]
+            )
+        )
+        if page.route == "/segunda":
+            page.views.append(
+                View(
+                    "/segunda",[
+                        AppBar(title=Text("segunda tela"), bgcolor=Colors.SECONDARY_CONTAINER)
+                        ]
+                )
+            )
+        page.update()
+    page.on_route_change = gerenciar_rotas
+    page.on_view_pop = voltar
+    page.on_route_change = gerenciar_rotas
+    page.go(page.route)
+
+
+ft.app(main)
